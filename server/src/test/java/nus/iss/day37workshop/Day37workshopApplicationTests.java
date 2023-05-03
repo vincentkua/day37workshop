036a3b27547bb3cadcf7c7aa74f2eb0970e75617
atlaskua <zylyn89@gmail.com>
2023-05-03 14:41:21 +0800

    initiated

---
+++ server/src/test/java/nus/iss/day37workshop/Day37workshopApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.day37workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day37workshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
